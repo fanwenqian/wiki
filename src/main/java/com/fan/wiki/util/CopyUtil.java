7113f7d1a112a5005cb907e34cc887413beffcdf
fan.wenqian <wqfan1231@sina.com>
2022-05-24 16:40:09 +0800

    增加类装转换工具类

---
+++ src/main/java/com/fan/wiki/util/CopyUtil.java
@@ -0,0 +1,55 @@
+package com.fan.wiki.util;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * bean转换工具类
+ *
+ * @author fan.wenqian
+ * @date 2022-05-24 16:04
+ */
+public class CopyUtil {
+
+    /**
+     * 单实体copy
+     * @param source 源实例
+     * @param clazz 返回类型
+     * @return 返回类型实体对象
+     */
+    public static <T> T copy(Object source, Class<T> clazz) {
+        if (source == null) {
+            return null;
+        }
+        T obj;
+        try {
+            obj = clazz.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        BeanUtils.copyProperties(source,obj);
+        return obj;
+    }
+
+    /**
+     * 实体List copy
+     * @param source 源实例
+     * @param clazz 返回List中存储的实例类型
+     * @return 实例List
+     */
+    public static <T> List<T> copy(List<T> source,Class<T> clazz) {
+        List<T> target = new ArrayList<>();
+        if (!CollectionUtils.isEmpty(source)) {
+            for (Object o : source) {
+                T obj = copy(o,clazz);
+                target.add(obj);
+            }
+        }
+        return target;
+    }
+}
