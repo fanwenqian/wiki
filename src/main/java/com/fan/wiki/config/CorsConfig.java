5238f95bc8ba3875983017d072eecc0ad08db764
fan.wenqian <wqfan1231@sina.com>
2022-05-26 17:50:16 +0800

    使用axios调用电子书查询列表接口

---
+++ src/main/java/com/fan/wiki/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.fan.wiki.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 跨域请求处理
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods(CorsConfiguration.ALL)
+                .allowCredentials(true)
+                .maxAge(3600);   //一小时内不再预检（发送OPTIONS请求）
+    }
+}
